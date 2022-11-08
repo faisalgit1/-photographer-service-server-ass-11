17fdbf400198e100e47ebaa91135c4f31ea489ca
faisalgit1 <mdfaisalahmed520@gmail.com>
2022-11-08 20:21:17 +0600

    use-middlewars

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
-
+const cors = require('cors')
 const app = express();
 const port = process.env.PORT || 5000;
+const data = require('./data/data.json');
 
+// middle wares
+app.use(cors());
+app.use(express.json());
+
+app.get('/data', (req, res) => {
+    res.send(data)
+});
 
 app.get('/', (req, res) => {
     res.send('Assignment server is running')
