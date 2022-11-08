9f76e85a653ede244be497cc3dd2870ef7107f0a
faisalgit1 <mdfaisalahmed520@gmail.com>
2022-11-08 13:23:22 +0600

    simple-check-api

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+app.get('/', (req, res) => {
+    res.send('Assignment server is running')
+})
+
+app.listen(port, () => {
+    console.log(`Assignment server running on ${port}`);
+})
