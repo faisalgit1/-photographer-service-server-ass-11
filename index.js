c7571daefd01ae97958ebd6892d7c7cb9983cf62
faisalgit1 <mdfaisalahmed520@gmail.com>
2022-11-09 14:46:15 +0600

    get-photos-data-from-mongodb

--- index.js
+++ index.js
@@ -15,7 +15,23 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+async function run() {
+    try {
+        const photoCollection = client.db('photoDb').collection('photos')
 
+        app.get('/chose-photos', async (req, res) => {
+            const query = {}
+            const cursor = photoCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err));
 
 
 app.get('/data', (req, res) => {
