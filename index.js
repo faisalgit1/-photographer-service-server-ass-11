0752d4c0eef01369fcf0ee5b26c31d4981e18754
faisalgit1 <mdfaisalahmed520@gmail.com>
2022-11-09 14:44:28 +0600

    connect-mongodb-and-set-user-and-password

--- index.js
+++ index.js
@@ -3,10 +3,20 @@ const cors = require('cors')
 const app = express();
 const port = process.env.PORT || 5000;
 const data = require('./data/data.json');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
 // middle wares
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.5bfvhe8.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
 
 app.get('/data', (req, res) => {
     res.send(data)
